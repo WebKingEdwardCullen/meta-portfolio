e523eb2466f047eab9bb4bfa351c195936a229bf
WebKingEdwardCullen <Webprogrammerking@gmail.com>
2022-09-09 13:28:06 +0500

    firts commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,29 @@
+var ourFounders = document.querySelector(".ourFounders");
+var partNers = document.querySelector(".partNers");
+var theFuture = document.querySelector(".theFuture");  
+
+var p1_nftt = document.querySelector(".p1_nftt");
+
+ourFounders.addEventListener("click",()=>{
+    p1_nftt.textContent = "META record label was founded by Shaky Lake and Baky Hike in 2022 business accelerator."
+    ourFounders.style.background = "#222222"
+    p1_nftt.style.color = "#8DFD1B"
+    partNers.style.background = "transparent"
+    theFuture.style.background = "transparent"
+});
+
+partNers.addEventListener("click",()=>{
+    p1_nftt.textContent = "Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli  "
+    partNers.style.background = "#222222"
+    ourFounders.style.background = "transparent"
+    p1_nftt.style.color = "#0d6efd"
+    theFuture.style.background = "transparent"  
+});
+
+theFuture.addEventListener("click",()=>{
+    p1_nftt.textContent = "Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali Guli Ali "
+    theFuture.style.background = "#222222"
+    ourFounders.style.background = "transparent"
+    partNers.style.background = "transparent"
+    p1_nftt.style.color = "cyan"
+}); 
